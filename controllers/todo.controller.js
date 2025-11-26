29e68da4e1afd9ac3bc2310e5587a7f22ac107fa
Bilol <83568251+Bilol03@users.noreply.github.com>
2025-11-26 16:31:48 +0500

    create todo finished

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -9,7 +9,7 @@
 //         }
 //     }
 // ]
-const { readFile } = require('../utils/fs.js')
+const { readFile, writeFile } = require('../utils/fs.js')
 
 const todos = readFile('todos.json')
 const users = readFile('users.json')
@@ -31,27 +31,52 @@ function getTodos(req, res) {
 
 function getTodoById(req, res) {
 	const id = req.params.id
-    const todo = todos.find(el => el.id == id)
-    let resTodo = {}
-    if(!todo) return res.json({
-        message: "todo not found"
-    })
-    const user = users.find(el => el.id == todo.user_id)
-    if(!user) return res.json({message: "User not found"})
-    resTodo['id'] = todo.id
-    resTodo['name'] = todo.name
-    resTodo['done'] = todo.done
-    resTodo['user'] = {
-        name: user.name,
-        phone: user.phone
+	const todo = todos.find((el) => el.id == id)
+	let resTodo = {}
+	if (!todo)
+		return res.json({
+			message: 'todo not found',
+		})
+	const user = users.find((el) => el.id == todo.user_id)
+	if (!user) return res.json({ message: 'User not found' })
+	resTodo['id'] = todo.id
+	resTodo['name'] = todo.name
+	resTodo['done'] = todo.done
+	resTodo['user'] = {
+		name: user.name,
+		phone: user.phone,
+	}
+	return res.json({
+		message: 'Success',
+		todo: resTodo,
+	})
+}
+
+function postTodo(req, res) {
+	const { name, user_id } = req.body
+	if (!(name && user_id))
+		return res.json({
+			message: 'name and user id required',
+		})
+    const user = users.find(el => el.id == user_id)
+    if(!user) res.json({status: 404, message: "User not found in this id"})
+    let newTodo = {
+        id: todos[todos.length - 1] ? todos[todos.length - 1].id + 1 : 1,
+        name,
+        done: false,
+        user_id
     }
+    todos.push(newTodo)
+    writeFile('todos.json', todos)
     return res.json({
+        status: 200,
         message: "Success",
-        todo: resTodo
+        newTodo
     })
 }
 
 module.exports = {
 	getTodos,
 	getTodoById,
+	postTodo,
 }
--- routes/todo.route.js
+++ routes/todo.route.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const {getTodos, getTodoById} = require('../controllers/todo.controller.js')
+const {getTodos, getTodoById, postTodo} = require('../controllers/todo.controller.js')
 
 const route = express.Router()
 
 route.get('/todos', getTodos)
 route.get('/todos/:id', getTodoById)
+route.post('/todos', postTodo)
 module.exports = route
