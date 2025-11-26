c4b0bfab653744ddb8dacef7753b73c083c4e9b0
Bilol <83568251+Bilol03@users.noreply.github.com>
2025-11-26 16:19:39 +0500

    get todo by id finished

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -9,28 +9,49 @@
 //         }
 //     }
 // ]
-const {readFile} = require('../utils/fs.js')
+const { readFile } = require('../utils/fs.js')
 
+const todos = readFile('todos.json')
+const users = readFile('users.json')
 function getTodos(req, res) {
-    const todos = readFile("todos.json")
-    const users = readFile('users.json')
-    let todoResult = []
-    let todo = {}
-    for (let i of todos) {
-        todo['id'] = i.id,
-        todo['name'] = i.name
-        todo['done'] = i.done,
-        todo['user'] = users.find(el => el.id == i.user_id)
+	let todoResult = []
+	let todo = {}
+	for (let i of todos) {
+		;(todo['id'] = i.id), (todo['name'] = i.name)
+		;(todo['done'] = i.done),
+			(todo['user'] = users.find((el) => el.id == i.user_id))
 
-        todoResult.push(todo)
+		todoResult.push(todo)
+	}
+	return res.json({
+		message: 'Success',
+		todoResult,
+	})
+}
+
+function getTodoById(req, res) {
+	const id = req.params.id
+    const todo = todos.find(el => el.id == id)
+    let resTodo = {}
+    if(!todo) return res.json({
+        message: "todo not found"
+    })
+    const user = users.find(el => el.id == todo.user_id)
+    if(!user) return res.json({message: "User not found"})
+    resTodo['id'] = todo.id
+    resTodo['name'] = todo.name
+    resTodo['done'] = todo.done
+    resTodo['user'] = {
+        name: user.name,
+        phone: user.phone
     }
     return res.json({
         message: "Success",
-        todoResult
+        todo: resTodo
     })
 }
 
-
 module.exports = {
-    getTodos
+	getTodos,
+	getTodoById,
 }
--- routes/todo.route.js
+++ routes/todo.route.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const {getTodos} = require('../controllers/todo.controller.js')
+const {getTodos, getTodoById} = require('../controllers/todo.controller.js')
 
 const route = express.Router()
 
 route.get('/todos', getTodos)
+route.get('/todos/:id', getTodoById)
 module.exports = route
